7f14bb648dee9588bbfe1167af42dd770376d2df
Luis Camilo Rosania Rivera <bamira1@misena.edu.co>
2023-11-03 11:34:13 -0500

    ok

--- analysis/analisisUsuarios.py
+++ analysis/analisisUsuarios.py
@@ -1,5 +1,16 @@
 from data.ListaUsuarios import usuarios
-from helpers.crearCSV import crearCSVUsuarios 
+from helpers.crearCSV import crearCSVUsuarios
+from helpers.crearHTML import crearTabla
+
+import pandas as pd
+
 
 #Usamos la funcion crearCSVusuarios
 crearCSVUsuarios(usuarios,'bdUsuarios.csv')
+
+#creando un dataframe desde una fuente CSV
+dataFrameUsuarios=pd.read_csv('data/bdUsuarios.csv')
+print(dataFrameUsuarios)
+
+#convertir el DF en TABLA HTML
+crearTabla(dataFrameUsuarios,'usuarios')
