f2c9c4b5b90fe3f46045b961ebe67069ac2a0f19
Luis Camilo Rosania Rivera <bamira1@misena.edu.co>
2023-11-17 11:32:46 -0500

    se crea query

--- analysis/analisisUsuarios.py
+++ analysis/analisisUsuarios.py
@@ -7,10 +7,11 @@ import pandas as pd
 
 #Usamos la funcion crearCSVusuarios
 crearCSVUsuarios(usuarios,'bdUsuarios.csv')
-
 #creando un dataframe desde una fuente CSV
 dataFrameUsuarios=pd.read_csv('data/bdUsuarios.csv')
-print(dataFrameUsuarios)
-
 #convertir el DF en TABLA HTML
 crearTabla(dataFrameUsuarios,'usuarios')
+
+#Se filtran los datos
+filtroUno=dataFrameUsuarios.query("(Edad>30) and (Nombre=='Jorge')")
+print(filtroUno)
